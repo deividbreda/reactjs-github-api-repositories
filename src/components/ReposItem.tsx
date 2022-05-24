9c221dadf5146ba83457802d95920bf5726eb8a4
Deivid Breda <deivid.breda02@gmail.com>
2022-05-24 16:33:47 -0300

    Implementação de mais props Github API

--- src/components/ReposItem.tsx
+++ src/components/ReposItem.tsx
@@ -7,8 +7,27 @@ interface RepositorioItem {
 export function ReposItem(props: RepositorioItem){
     return(
         <>
-        <h1>{props.repositorio.name}</h1>
-        <p>{props.repositorio.description}</p>
+        <div className="col col-2">
+            <div className="itemConteudoRepositorio">
+                <div className="col">
+                    <div className="col col-2-itemTituloRepositorio">
+                        <div className="itemConteudoTituloRepositorio">
+                            <a className="AnchorRepositorio" href={props.repositorio.html_url} target="_blank">
+                                <h1 className="tituloItemRepositorio">{props.repositorio.name}</h1>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="col col-2-itemStatusRepositorio">
+                        <div className="itemConteudoTituloRepositorio">
+                            <p className="statusItemRepositorio"> {props.repositorio.visibility} </p>
+                        </div>
+                    </div>
+                </div>
+                <p className="descricaoItemRepositorio">{props.repositorio.description}</p>
+                <p className="linguageItemRepositorio"> {props.repositorio.language } </p>
+            </div>
+        </div>
         </>
     );
 }
--- src/components/ReposList.tsx
+++ src/components/ReposList.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import { ReposItem } from "./ReposItem";
+import logo from '../images/logo.png';
+import perfil from '../images/perfil.jpg';
 
 export interface Repositorio {
     name: string,
     description: string,
+    html_url: string,
+    language: string,
+    visibility: string,
 }
 
 export function ReposList(){
@@ -15,12 +20,45 @@ export function ReposList(){
             .then(data => setRepositorios(data))
     }, [repositorios])
 
-    return(
+    return( 
         <>
-        <h1>Lista de Repositórios</h1>
-        {repositorios.map(repositorio => {
-            return <ReposItem key={repositorio.name} repositorio={repositorio} />
-        })}
+        <div id="cabecalho">
+            <div className="containerApp">
+                <div className="flexCentralizado containerFlexCentralizado">
+                    <img src={logo} alt="" className="logo" />
+                </div>
+            </div>      
+        </div>
+
+        <div id="home">
+            <div className="containerApp">
+                <div className="col">
+                    <div className="col col-2-perfil">
+                        <div className="allItemHome">
+                            <img className="fotoPerfil" src={perfil} alt="" />
+                        </div>
+                    </div>
+
+                    <div className="col col-2-repositorios">
+                        <div className="allItemHome">
+                            <div className="itemConteudoInput">
+                                <input className="inputPesquisa" placeholder="Pesquisar repositório..." type="text" name="" id="" />
+                            </div>
+
+                            <h1 className="tituloRespositorios">Lista de Repositórios</h1>
+                            <div className="allConteudoRepositorios">
+                                <div className="col">
+                            {repositorios.map(repositorio => {
+                                return <ReposItem key={repositorio.name} repositorio={repositorio} />
+                            })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
         </>
     );
 }
